dbdb317b9520eebc04abb9e424009bda869e9532
Farad2020 <fnirc@mail.ru>
2021-04-26 17:22:30 +0600

    big upd with proj

---
+++ GameGlamer/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using GameGlamer.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameGlamer.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+
+        public DbSet<GameDeal> Courses { get; set; }
+        public DbSet<DLCorLootDeal> Enrollments { get; set; }
+    }
+}
