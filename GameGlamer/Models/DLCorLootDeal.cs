9d30367bb7a95ee980312dfd54916e375891a34f
Farad2020 <fnirc@mail.ru>
2021-05-06 12:06:50 +0600

    Loot Details upd

--- GameGlamer/Models/DLCorLootDeal.cs
+++ GameGlamer/Models/DLCorLootDeal.cs
@@ -9,18 +9,38 @@ namespace GameGlamer.Models
     public class DLCorLootDeal
     {
         public int id { get; set; }
+
+        [System.ComponentModel.DisplayName("Title")]
         public string title { get; set; }
+
+        [System.ComponentModel.DisplayName("Worth")]
         public string worth { get; set; }
         public string thumbnail { get; set; }
         public string image { get; set; }
+
+        [System.ComponentModel.DisplayName("Description")]
         public string description { get; set; }
         public string instructions { get; set; }
+
+        [System.ComponentModel.DisplayName("Giveaway Source")]
         public string open_giveaway_url { get; set; }
+
+        [System.ComponentModel.DisplayName("Published Date")]
         public string published_date { get; set; }
+
+        [System.ComponentModel.DisplayName("Type")]
         public string type { get; set; }
+
+        [System.ComponentModel.DisplayName("Platforms")]
         public string platforms { get; set; }
+
+        [System.ComponentModel.DisplayName("End Date")]
         public string end_date { get; set; }
+
+        [System.ComponentModel.DisplayName("Users")]
         public int users { get; set; }
+
+        [System.ComponentModel.DisplayName("Status")]
         public string status { get; set; }
         public string gamerpower_url { get; set; }
         public string open_giveaway { get; set; }
